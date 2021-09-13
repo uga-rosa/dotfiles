964688272b31754ef4b2e256e72ad064b6a28e2f
uga-rosa <uga6603@gmail.com>
2021-09-14 02:21:23 +0900

    lspsaga

--- .config/nvim/lua/plugins/config/lsp.lua
+++ .config/nvim/lua/plugins/config/lsp.lua
@@ -97,11 +97,17 @@ augroup({
 })
 
 saga.init_lsp_saga({
-  use_saga_diagnostic_sign = true,
   error_sign = " ",
   warn_sign = " ",
   hint_sign = " ",
   infor_sign = " ",
+  code_action_prompt = {
+    enable = false,
+  },
+  rename_action_keys = {
+    quit = { "<C-c>", "<esc>" },
+    exec = "<cr>",
+  },
 })
 
 local action = require("lspsaga.action")
