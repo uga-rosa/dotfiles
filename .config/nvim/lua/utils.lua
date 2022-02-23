fdf118f3c1a2c055d4c3c54518a368000323877e
uga-rosa <uga6603@gmail.com>
2022-02-24 02:05:44 +0900

    vim-maketable

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -1,39 +1,7 @@
-local api = vim.api
-
 _G.utils = {}
 
-if not vim.keymap then
-    vim.keymap = {}
-    vim.keymap._function = {}
-    function vim.keymap.set(mode, lhs, rhs, opt)
-        if type(rhs) == "function" then
-            local idx = #vim.keymap._function + 1
-            vim.keymap._function[idx] = rhs
-            rhs = ("<cmd>lua vim.keymap._function[%d]()<cr>"):format(idx)
-        end
-        mode = type(mode) == "table" and mode or {mode}
-        for _, m in ipairs(mode) do
-            api.nvim_set_keymap(m, lhs, rhs, opt)
-        end
-    end
-end
-
----API for keymap
----@param mode string|table
----@param lhs string
----@param rhs string|function
----@param opt list
-function utils.map(mode, lhs, rhs, opt)
-    opt = type(opt) == "table" and opt or { opt }
-    for i, o in ipairs(opt) do
-        opt[o] = true
-        opt[i] = nil
-    end
-    vim.keymap.set(mode, lhs, rhs, opt)
-end
-
 function utils.feedkey(key, mode)
-    api.nvim_feedkeys(api.nvim_replace_termcodes(key, true, true, true), mode or "n", true)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode or "n", true)
 end
 
 ---Transforms ctx into a human readable representation.
