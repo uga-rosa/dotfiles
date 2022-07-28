e6fc0ab4bc42ba3b6ce34167a5805d2312e91740
uga-rosa <uga6603@gmail.com>
2022-07-28 10:03:33 +0900

    update

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -1,7 +1,17 @@
+local uv = vim.loop
+
 ---Transforms ctx into a human readable representation.
 ---@vararg any
 function _G.dump(...)
     for _, ctx in ipairs({ ... }) do
         print(vim.inspect(ctx))
     end
+end
+
+function _G.readfile(path)
+    local fd = assert(uv.fs_open(path, "r", tonumber("0444", 8)))
+    local stat = assert(uv.fs_stat(fd))
+    local buf = assert(uv.fs_read(fd, stat.size, 0))
+    uv.fs_close(fd)
+    return buf
 end
