4683d7b9342205748c0c9631bfa324957446f22b
uga-rosa <uga6603@gmail.com>
2022-08-26 08:55:39 +0900

    ftdetect: use vim.filetype

--- .config/nvim/lua/rc/ftdetect.lua
+++ .config/nvim/lua/rc/ftdetect.lua
@@ -1,21 +1,6 @@
-local M = {
-    {
-        pattern = "*.ahk",
-        filetype = "autohotkey",
+vim.filetype.add({
+    extension = {
+        ahk = "autohotkey",
+        inp = "packmol",
     },
-    {
-        pattern = "*.inp",
-        filetype = "packmol",
-    },
-}
-
-local group_name = "ftdetect"
-vim.api.nvim_create_augroup(group_name, {})
-for _, ftdetect in ipairs(M) do
-    local pattern = ftdetect.pattern
-    local filetype = ftdetect.filetype
-    vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-        pattern = pattern,
-        command = "setf " .. filetype,
-    })
-end
+})
