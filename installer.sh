2197175c10573183005db8d4d5729f789d0851bd
uga-rosa <uga6603@gmail.com>
2022-11-13 00:16:00 +0900

    remove DS_Store

--- installer.sh
+++ installer.sh
@@ -7,6 +7,5 @@ DOTFILES_PATH=$(cd $(dirname $0); pwd)
 for i in .??*; do
   [[ $i == ".git" ]] && continue
   [[ $i == ".gitignore" ]] && continue
-  [[ $i == ".DS_Store" ]] && continue
   ln -snfv "${DOTFILES_PATH}"/"$i" ~/"$i"
 done
