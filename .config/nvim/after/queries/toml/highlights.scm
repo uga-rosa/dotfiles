974a76c004a613649da90c6e6512c9d3f5723aca
uga-rosa <uga6603@gmail.com>
2022-11-14 19:08:39 +0900

    better query for dein.toml

--- .config/nvim/after/queries/toml/highlights.scm
+++ .config/nvim/after/queries/toml/highlights.scm
@@ -2,22 +2,26 @@
 ((pair
   (bare_key) @_key
   (string) @none)
- (#vim-match? @_key "^hook_\w*")
+ (#vim-match? @_key "^%(hook|lua)_\w*")
+ (#vim-match? @none "^('''|\"\"\")")
  (#offset! @none 0 3 0 -3))
 ((pair
   (bare_key) @_key
   (string) @none)
- (#vim-match? @_key "^lua_\w*")
+ (#vim-match? @_key "^%(hook|lua)_\w*")
+ (#vim-match? @none "^['\"][^'\"]")
+ (#offset! @none 0 1 0 -1))
+((table
+  (bare_key) @_key
+  (pair
+   (string) @none))
+ (#vim-match? @_key "^%(plugins\.)?ftplugin$")
+ (#vim-match? @none "^('''|\"\"\")")
  (#offset! @none 0 3 0 -3))
 ((table
   (bare_key) @_key
   (pair
    (string) @none))
- (#eq? @_key "ftplugin")
- (#offset! @none 0 3 0 -3))
-((table
-  (dotted_key) @_key
-  (pair
-   (string) @none))
- (#eq? @_key "plugins.ftplugin")
- (#offset! @none 0 3 0 -3))
+ (#vim-match? @_key "^%(plugins\.)?ftplugin$")
+ (#vim-match? @none "^['\"][^'\"]")
+ (#offset! @none 0 1 0 -1))
