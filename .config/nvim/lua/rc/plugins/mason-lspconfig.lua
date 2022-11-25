28129e3745beeb94527ba2f2e0436e905d3ee218
uga-rosa <uga6603@gmail.com>
2022-11-26 01:38:48 +0900

    improve lsp diagnostics

--- .config/nvim/lua/rc/plugins/mason-lspconfig.lua
+++ .config/nvim/lua/rc/plugins/mason-lspconfig.lua
@@ -85,3 +85,13 @@ require("mason-lspconfig").setup_handlers({
 
 -- Deno
 lspconfig.denols.setup(opts)
+
+-- Diagnostics
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  update_in_insert = false,
+  virtual_text = {
+    format = function(diagnostic)
+      return string.format("%s (%s: %s)", diagnostic.message, diagnostic.source, diagnostic.code)
+    end,
+  },
+})
