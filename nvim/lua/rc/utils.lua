4ff41e0e9c54fdf59932df7f28512e5254619f12
uga-rosa <uga6603@gmail.com>
2023-03-15 18:23:59 +0900

    skkeleton config in lua

--- nvim/lua/rc/utils.lua
+++ nvim/lua/rc/utils.lua
@@ -1,4 +1,4 @@
-_G.dump = vim.pretty_print
+local uv = vim.loop
 
 ---@type table<string, fun(...: unknown): boolean>
 vim.bool_fn = setmetatable({}, {
@@ -14,3 +14,20 @@ vim.bool_fn = setmetatable({}, {
     end
   end,
 })
+
+---@param fname string
+---@return string
+function vim.fs.read(fname)
+  local fd = assert(uv.fs_open(fname, "r", 292)) -- 0444
+  local stat = assert(uv.fs_fstat(fd))
+  local buffer = assert(uv.fs_read(fd, stat.size, 0))
+  uv.fs_close(fd)
+  return buffer
+end
+
+---@param fname string
+---@param data string|string[]
+function vim.fs.write(fname, data)
+  local fd = assert(uv.fs_open(fname, "w", 438))
+  assert(uv.fs_write(fd, data))
+end
