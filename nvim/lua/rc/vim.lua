93371125ae9e23a2faf6dd7aa233ae0c4f079832
uga-rosa <uga6603@gmail.com>
2023-04-25 20:45:11 +0900

    use vim.fn.empty to determine truthy/falsy in vim script

--- nvim/lua/rc/vim.lua
+++ nvim/lua/rc/vim.lua
@@ -5,12 +5,7 @@ vim.bool_fn = setmetatable({}, {
   __index = function(_, key)
     return function(...)
       local v = vim.fn[key](...)
-      if not v or v == 0 or v == "" then
-        return false
-      elseif type(v) == "table" and next(v) == nil then
-        return false
-      end
-      return true
+      return vim.fn.empty(v) == 0
     end
   end,
 })
