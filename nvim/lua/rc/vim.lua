ae98f66a3d210f774b4b1a254a3c6ed0ffa22386
uga-rosa <uga6603@gmail.com>
2023-04-27 17:24:04 +0900

    add vim.fs.(exists|isfile|isdir)

--- nvim/lua/rc/vim.lua
+++ nvim/lua/rc/vim.lua
@@ -10,6 +10,41 @@ vim.bool_fn = setmetatable({}, {
   end,
 })
 
+---@param path string
+---@return table|nil
+local function get_stat(path)
+  ---@type string|nil
+  local fullpath = vim.loop.fs_realpath(vim.fs.normalize(path))
+  return fullpath and vim.loop.fs_stat(fullpath)
+end
+
+---Returns whether path exists or not.
+---In case of symlink, refer to the attribute of the link destination.
+---@param path string
+---@return boolean
+function vim.fs.exists(path)
+  local stat = get_stat(path)
+  return stat ~= nil
+end
+
+---Returns whether path indicates a file.
+---In case of symlink, refer to the attribute of the link destination.
+---@param path string
+---@return boolean
+function vim.fs.isfile(path)
+  local stat = get_stat(path)
+  return stat ~= nil and stat.type == "file"
+end
+
+---Returns whether path indicates a directory.
+---In case of symlink, refer to the attribute of the link destination.
+---@param path string
+---@return boolean
+function vim.fs.isdir(path)
+  local stat = get_stat(path)
+  return stat ~= nil and stat.type == "directory"
+end
+
 ---@param fname string
 ---@return string
 function vim.fs.read(fname)
