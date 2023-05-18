532f1da0e81c4a18a99b592a29e16dbe548f3632
uga-rosa <uga6603@gmail.com>
2023-05-19 00:54:42 +0900

    utility for ddu

--- nvim/lua/rc/ddu.lua
+++ nvim/lua/rc/ddu.lua
@@ -13,4 +13,49 @@ function M.start(config)
   vim.fn["ddu#start"](config)
 end
 
+---@param name string
+---@param params table?
+---@return function
+function M.call_action(name, params)
+  return function()
+    vim.fn["ddu#ui#ff#do_action"](name, params or vim.empty_dict())
+  end
+end
+
+---@param name string
+---@param params table?
+---@return function
+function M.itemAction(name, params)
+  local opts = {
+    name = name,
+    params = params,
+  }
+  return M.call_action("itemAction", opts)
+end
+
+---@return function
+function M.open_tab()
+  return function()
+    M.itemAction("open", { command = "tabedit" })()
+    local root = vim
+      .iter(vim.fs.find({ "init.vim", ".git" }, {
+        upward = true,
+      }))
+      :map(vim.fs.dirname)
+      :totable()[1]
+    if root then
+      vim.cmd.tcd(root)
+    end
+  end
+end
+
+---@param cmd string
+---@return function
+function M.execute(cmd)
+  return function()
+    vim.fn["ddu#ui#ff#execute"](cmd)
+    vim.cmd("redraw!")
+  end
+end
+
 return M
