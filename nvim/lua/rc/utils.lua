9b3b4fce4c0c2d88e08bd1d8ed9af61fcc690d5a
uga-rosa <uga6603@gmail.com>
2023-05-21 14:38:00 +0900

    merge rc/init.lua -> rc/utils.lua

--- nvim/lua/rc/utils.lua
+++ nvim/lua/rc/utils.lua
@@ -1,5 +1,21 @@
 local M = {}
 
+---@param cmd string|function
+function M.keep_cursor(cmd)
+  local win_id = vim.api.nvim_get_current_win()
+  local view = vim.fn.winsaveview()
+  pcall(function()
+    if type(cmd) == "string" then
+      vim.cmd(cmd)
+    elseif type(cmd) == "function" then
+      cmd()
+    end
+  end)
+  if vim.api.nvim_get_current_win() == win_id then
+    vim.fn.winrestview(view)
+  end
+end
+
 ---@param path string?
 ---@return string? root_dir
 function M.find_root(path)
@@ -23,6 +39,16 @@ function M.package_set(package_name, key, value)
   module[key] = value
   package.preload[package_name] = function()
     return module
+  end
+end
+
+---@param fn function
+---@param ... unknown Arguments
+---@return function
+function M.createWrapper(fn, ...)
+  local args = { ... }
+  return function()
+    fn(unpack(args))
   end
 end
 
