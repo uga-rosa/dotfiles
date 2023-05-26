9952573478b663d53d26585c7293b4430cc463f2
uga-rosa <uga6603@gmail.com>
2023-05-26 17:37:42 +0900

    remove winbar from heirline

--- nvim/lua/rc/heirline/init.lua
+++ nvim/lua/rc/heirline/init.lua
@@ -1,26 +1,8 @@
 local heirline = require("heirline")
 
----@param winid integer?
----@return boolean
-local function is_float_win(winid)
-  local config = vim.api.nvim_win_get_config(winid or 0)
-  return config.relative ~= ""
-end
-
-local function is_disable_buf(bufid)
-  local filetype = vim.api.nvim_buf_get_option(bufid, "filetype")
-  return ({
-    fern = true,
-    ugaterm = true,
-  })[filetype]
-end
-
 heirline.setup({
   opts = {
     colors = require("rc.heirline.colors"),
-    disable_winbar_cb = function(arg)
-      return is_float_win() or is_disable_buf(arg.buf)
-    end,
   },
   statusline = {
     -- global setting
@@ -45,8 +27,5 @@ heirline.setup({
       provider = "%7(%l/%3L%):%2c %P",
       hl = { fg = "bg", bg = "fg" },
     },
-  },
-  winbar = {
-    require("rc.heirline.navic"),
   },
 })
