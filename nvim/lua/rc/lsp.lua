b322625220138437d35abd785db76c4d4db462d1
uga-rosa <uga6603@gmail.com>
2023-05-26 17:39:00 +0900

    barbecue holiday

---
+++ nvim/lua/rc/lsp.lua
@@ -0,0 +1,43 @@
+local M = {}
+
+---Register callback function on LspAttach
+---@param callback fun(client, bufnr)
+function M.on_attach(callback)
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local bufnr = args.buf
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      callback(client, bufnr)
+    end,
+  })
+end
+
+M.kind = {
+  Text = "",
+  Method = "",
+  Function = "",
+  Constructor = "",
+  Field = "ﰠ",
+  Variable = "",
+  Class = "ﴯ",
+  Interface = "",
+  Module = "",
+  Property = "ﰠ",
+  Unit = "塞",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "פּ",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
+}
+
+return M
