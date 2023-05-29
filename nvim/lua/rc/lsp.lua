028ef391d84795aa888ce8bbbb087f7b0afa6d6f
uga-rosa <uga6603@gmail.com>
2023-05-29 13:25:25 +0900

    move kind

--- nvim/lua/rc/lsp.lua
+++ nvim/lua/rc/lsp.lua
@@ -12,32 +12,4 @@ function M.on_attach(callback)
   })
 end
 
-M.kind = {
-  Text = "󰉿",
-  Method = "󰆧",
-  Function = "󰊕",
-  Constructor = "",
-  Field = "󰜢",
-  Variable = "󰀫",
-  Class = "󰠱",
-  Interface = "",
-  Module = "",
-  Property = "󰜢",
-  Unit = "󰑭",
-  Value = "󰎠",
-  Enum = "",
-  Keyword = "󰌋",
-  Snippet = "",
-  Color = "󰏘",
-  File = "󰈙",
-  Reference = "󰈇",
-  Folder = "󰉋",
-  EnumMember = "",
-  Constant = "󰏿",
-  Struct = "󰙅",
-  Event = "",
-  Operator = "󰆕",
-  TypeParameter = "",
-}
-
 return M
