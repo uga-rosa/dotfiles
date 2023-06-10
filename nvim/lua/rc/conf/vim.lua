68deb076656af496bdcc857a5d0d91cf89f5bd3b
uga-rosa <uga6603@gmail.com>
2023-06-10 19:59:17 +0900

    Fix: need vim.deepcopy(); list_extend mutates dst

--- nvim/lua/rc/conf/vim.lua
+++ nvim/lua/rc/conf/vim.lua
@@ -67,16 +67,12 @@ local meta = {}
 
 meta.__call = function(self, ...)
   local keys = rawget(self, "_keys")
-  if #keys < 2 then
-    vim.notify("invalid usage: you can use vim.fn instead", vim.log.levels.ERROR)
-    return
-  end
   return vim.fn[table.concat(keys, "#")](...)
 end
 
 meta.__index = function(self, key)
   local keys = rawget(self, "_keys") or {}
-  return setmetatable({ _keys = vim.list_extend(keys, { key }) }, meta)
+  return setmetatable({ _keys = vim.list_extend(vim.deepcopy(keys), { key }) }, meta)
 end
 
 --- autoload function wrapper
