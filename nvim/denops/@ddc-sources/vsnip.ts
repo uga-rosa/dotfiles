43886d30943e75dde5ed5379f05d229b8d15b744
uga-rosa <uga6603@gmail.com>
2023-06-28 22:38:56 +0900

    Expand snippet in onCompleteDone

--- nvim/denops/@ddc-sources/vsnip.ts
+++ nvim/denops/@ddc-sources/vsnip.ts
@@ -1,20 +1,51 @@
 import { BaseSource, Item } from "https://deno.land/x/ddc_vim@v3.6.0/types.ts";
 import { Denops, fn } from "https://deno.land/x/ddc_vim@v3.6.0/deps.ts";
+import { OnCompleteDoneArguments } from "https://deno.land/x/ddc_vim@v3.6.0/base/source.ts";
+
+type MetaData = {
+  vsnip: {
+    snippet: string[];
+  };
+};
 
 type Params = Record<string, never>;
 
 export class Source extends BaseSource<Params> {
   async gather(args: {
     denops: Denops;
-  }): Promise<Item[]> {
+  }): Promise<Item<MetaData>[]> {
     const items = await args.denops.call(
       "vsnip#get_complete_items",
       await fn.bufnr(args.denops),
-    ) as Item[];
+    ) as Item<string>[];
     return items.map((item) => ({
       ...item,
       menu: undefined,
+      user_data: JSON.parse(item.user_data ?? "{}"),
     }));
+  }
+
+  async onCompleteDone({
+    denops,
+  }: OnCompleteDoneArguments<Params, MetaData>): Promise<void> {
+    // No expansion unless confirmed by pum#map#confirm()
+    const itemWord = await denops.eval(`v:completed_item.word`) as string;
+    const ctx = await this.lineContext(denops);
+    if (!ctx.text.endsWith(itemWord, ctx.character)) {
+      return;
+    }
+    await denops.call("vsnip#expand");
+  }
+
+  private async lineContext(
+    denops: Denops,
+  ) {
+    const beforeLine = await denops.eval(
+      `getline(".")[:getcurpos()[2]-2]`,
+    ) as string;
+    const character = beforeLine.length;
+    const text = await fn.getline(denops, ".");
+    return { character, text };
   }
 
   params(): Params {
