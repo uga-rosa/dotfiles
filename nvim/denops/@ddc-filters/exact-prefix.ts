ff69c6a4728215178d3e2ee27a2c268ad7039914
uga-rosa <uga6603@gmail.com>
2023-06-30 16:45:10 +0900

    filter-exact-prefix

---
+++ nvim/denops/@ddc-filters/exact-prefix.ts
@@ -0,0 +1,24 @@
+import { BaseFilter, FilterArguments, Item } from "../rc/deps.ts";
+
+type Params = {
+  length: number;
+};
+
+export class Filter extends BaseFilter<Params> {
+  override filter({
+    items,
+    completeStr,
+    filterParams,
+  }: FilterArguments<Params>): Promise<Item[]> {
+    const prefix = completeStr.slice(0, filterParams.length);
+    return Promise.resolve(
+      items.filter((item) => item.word.startsWith(prefix)),
+    );
+  }
+
+  override params(): Params {
+    return {
+      length: 1,
+    };
+  }
+}
