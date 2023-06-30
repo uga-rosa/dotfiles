c2baab5b8e64012c3347f2c114e28cedaad7b264
uga-rosa <uga6603@gmail.com>
2023-06-30 23:47:55 +0900

    Remove Promise.resolve

--- nvim/denops/@ddc-sources/nvim-lua.ts
+++ nvim/denops/@ddc-sources/nvim-lua.ts
@@ -18,31 +18,27 @@ export class Source extends BaseSource<Params> {
   async gather({
     denops,
   }: GatherArguments<Params>): Promise<DdcGatherItems> {
-    return await Promise.resolve().then(async () => {
-      const ctx = await LineContext.create(denops);
-      const [path] = ctx.text.slice(0, ctx.character).match(/\w[\w.]*$/) ?? [];
-      if (path === undefined) {
-        throw new Error("");
-      }
-      return path;
-    }).then(async (path) => {
-      const parent = path.split(".");
-      parent.pop();
-      const items = await denops.call(
-        "luaeval",
-        `require("rc.ddc_sources.nvim_lua").items(_A)`,
-        parent,
-      ) as Item[];
-      return { items, parent: parent.join(".") };
-    }).then(({ items, parent }) => {
-      return items.map((item) => ({
-        ...item,
-        user_data: {
-          name: item.word,
-          fullpath: `${parent}.${item.word}`,
-        },
-      })) satisfies Item<UserData>[];
-    }).catch(() => []);
+    const ctx = await LineContext.create(denops);
+    const [path] = ctx.text.slice(0, ctx.character).match(/\w[\w.]*$/) ?? [];
+    if (path === undefined) {
+      return [];
+    }
+    const parent = path.split(".");
+    parent.pop();
+
+    const items = await denops.call(
+      "luaeval",
+      `require("rc.ddc_sources.nvim_lua").items(_A)`,
+      parent,
+    ) as Item[];
+
+    return items.map((item) => ({
+      ...item,
+      user_data: {
+        name: item.word,
+        fullpath: `${parent.join(".")}.${item.word}`,
+      },
+    }));
   }
 
   async onCompleteDone({
