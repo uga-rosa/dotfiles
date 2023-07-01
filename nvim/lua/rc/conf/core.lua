ba9b17d063ef3cdc8fa52972f506f9aa2fc13bd2
uga-rosa <uga6603@gmail.com>
2023-07-01 23:29:48 +0900

    load secret keys

--- nvim/lua/rc/conf/core.lua
+++ nvim/lua/rc/conf/core.lua
@@ -3,3 +3,7 @@ require("rc.conf.mappings")
 require("rc.conf.vim")
 require("rc.conf.ftdetect")
 require("rc.conf.lsp")
+
+if vim.fs.isfile("~/.secret/key.vim") then
+  vim.cmd.source("~/.secret/key.vim")
+end
