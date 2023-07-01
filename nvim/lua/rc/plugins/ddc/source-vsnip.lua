ad720b2a8ffbc76b7236418d54b25f30c71aac0f
uga-rosa <uga6603@gmail.com>
2023-07-01 23:29:20 +0900

    Make plugin ddc-source-vsnip

--- nvim/lua/rc/plugins/ddc/source-vsnip.lua
+++ nvim/lua/rc/plugins/ddc/source-vsnip.lua
@@ -2,8 +2,12 @@ local helper = require("rc.helper.ddc")
 
 ---@type LazySpec
 local spec = {
-  "hrsh7th/vim-vsnip",
-  dependencies = "ddc.vim",
+  "uga-rosa/ddc-source-vsnip",
+  dev = true,
+  dependencies = {
+    "ddc.vim",
+    "hrsh7th/vim-vsnip",
+  },
   init = function()
     vim.g.vsnip_snippet_dir = vim.fn.stdpath("config") .. "/snippets"
     vim.g.vsnip_choice_delay = 200
@@ -12,6 +16,9 @@ local spec = {
     helper.patch_global({
       sourceOptions = {
         vsnip = { mark = "[Vsnip]" },
+      },
+      sourceParams = {
+        vsnip = { menu = false },
       },
     })
   end,
