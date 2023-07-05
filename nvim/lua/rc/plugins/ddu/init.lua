9e0b242d937ff82b859a547540189fb6d9677f3f
uga-rosa <uga6603@gmail.com>
2023-07-05 17:53:19 +0900

    Move mapping depending on ui-ff

--- nvim/lua/rc/plugins/ddu/init.lua
+++ nvim/lua/rc/plugins/ddu/init.lua
@@ -5,31 +5,12 @@ helper.ff_map(nil, function(map)
   vim.opt_local.cursorline = true
   -- Default itemAction
   map("<CR>", helper.item_action("default"))
-  -- Enter filter
-  map("i", helper.action("openFilterWindow"))
-  -- Close UI
-  map("<Esc>", helper.action("quit"))
-  -- Toggle selected state for all items
-  map("a", helper.action("toggleAllItems"))
-  -- Toggle selected state for cursor item
-  map(" ", helper.action("toggleSelectItem"))
-  -- Expand item tree
-  map("e", helper.action("expandItem", { mode = "toggle" }))
-  -- Show available actions
-  map("+", helper.action("chooseAction"))
 end)
 
 helper.ff_filter_map(nil, function(map)
   -- Default itemAction
   map("i", "<CR>", helper.item_action("default", nil, true))
   map("n", "<CR>", helper.item_action("default"))
-  -- Close UI
-  map("i", "<C-c>", helper.action("quit", nil, true))
-  -- Close filter window
-  map("n", "<Esc>", helper.action("closeFilterWindow", nil, true))
-  -- Move cursor
-  map("i", "<C-n>", helper.execute("normal j"))
-  map("i", "<C-p>", helper.execute("normal k"))
 end)
 
 vim.api.nvim_create_user_command("Ddu", function(args)
--- nvim/lua/rc/plugins/ddu/ui-ff.lua
+++ nvim/lua/rc/plugins/ddu/ui-ff.lua
@@ -1,5 +1,30 @@
 local helper = require("rc.helper.ddu")
 
+helper.ff_map(nil, function(map)
+  -- Enter filter
+  map("i", helper.action("openFilterWindow"))
+  -- Close UI
+  map("<Esc>", helper.action("quit"))
+  -- Toggle selected state for all items
+  map("a", helper.action("toggleAllItems"))
+  -- Toggle selected state for cursor item
+  map(" ", helper.action("toggleSelectItem"))
+  -- Expand item tree
+  map("e", helper.action("expandItem", { mode = "toggle" }))
+  -- Show available actions
+  map("+", helper.action("chooseAction"))
+end)
+
+helper.ff_filter_map(nil, function(map)
+  -- Close UI
+  map("i", "<C-c>", helper.action("quit", nil, true))
+  -- Close filter window
+  map("n", "<Esc>", helper.action("closeFilterWindow", nil, true))
+  -- Move cursor
+  map("i", "<C-n>", helper.execute("normal j"))
+  map("i", "<C-p>", helper.execute("normal k"))
+end)
+
 ---@type LazySpec
 local spec = {
   "Shougo/ddu-ui-ff",
