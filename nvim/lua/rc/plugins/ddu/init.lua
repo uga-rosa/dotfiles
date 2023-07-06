c75280c09bf7693bd6ef7d4f7c8f8adfda87e416
uga-rosa <uga6603@gmail.com>
2023-07-06 12:10:23 +0900

    pre-load hack

--- nvim/lua/rc/plugins/ddu/init.lua
+++ nvim/lua/rc/plugins/ddu/init.lua
@@ -34,6 +34,9 @@ local spec = {
   name = "ddu.vim",
   dependencies = "vim-denops/denops.vim",
   import = "rc.plugins.ddu",
+  config = function()
+    vim.schedule(vim.fn["ddu#start"])
+  end,
 }
 
 return spec
