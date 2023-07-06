690db68bc132b0208047d5809b2292233c9d73ae
uga-rosa <uga6603@gmail.com>
2023-07-06 13:45:29 +0900

    Gather buffers from the current tab

--- nvim/lua/rc/plugins/ddc/source-buffer.lua
+++ nvim/lua/rc/plugins/ddc/source-buffer.lua
@@ -15,7 +15,7 @@ local spec = {
           getBufnrs = helper.register(function()
             -- Visible buffers
             return vim
-              .iter(vim.api.nvim_list_wins())
+              .iter(vim.api.nvim_tabpage_list_wins(0))
               :map(function(win)
                 return vim.api.nvim_win_get_buf(win)
               end)
