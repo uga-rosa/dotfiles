559b676f5d3765395015ea49e7d3c44539cf3d3a
uga-rosa <uga6603@gmail.com>
2023-08-01 21:25:18 +0900

    source-zenn_dev

--- nvim/lua/rc/plugins/ddu/source-zenn.lua
+++ nvim/lua/rc/plugins/ddu/source-zenn.lua
@@ -2,19 +2,21 @@ local helper = require("rc.helper.ddu")
 
 ---@type LazySpec
 local spec = {
-  "mikanIchinose/ddu-source-zenn",
+  "kyoh86/ddu-source-zenn_dev",
   dependencies = "ddu.vim",
   init = function()
     vim.keymap.set("n", "<Space>z", "<Cmd>Ddu zenn<CR>")
   end,
   config = function()
     helper.register("zenn", function()
-      helper.start("file", "zenn", {
-        sourceOptions = {
-          zenn = {
-            path = vim.fs.normalize("~/zenn"),
-            converters = {},
-          },
+      helper.start("file", {
+        "zenn_dev_article",
+        params = {
+          slug = false,
+        },
+        options = {
+          path = vim.fs.normalize("~/zenn"),
+          converters = {},
         },
       })
     end)
