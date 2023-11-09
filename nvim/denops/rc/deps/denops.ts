9c2c0de08a0c036b3cb0cad724dddd027d3bf853
uga-rosa <uga6603@gmail.com>
2023-11-09 09:26:04 +0900

    Update dependencies

--- nvim/denops/rc/deps/denops.ts
+++ nvim/denops/rc/deps/denops.ts
@@ -1,7 +1,7 @@
-export { type Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
-export * as nvim from "https://deno.land/x/denops_std@v5.0.1/function/nvim/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v5.0.1/option/mod.ts";
-export { batch } from "https://deno.land/x/denops_std@v5.0.1/batch/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v5.0.1/autocmd/mod.ts";
-export * as lambda from "https://deno.land/x/denops_std@v5.0.1/lambda/mod.ts";
+export { type Denops } from "https://deno.land/x/denops_std@v5.0.2/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v5.0.2/function/mod.ts";
+export * as nvim from "https://deno.land/x/denops_std@v5.0.2/function/nvim/mod.ts";
+export * as op from "https://deno.land/x/denops_std@v5.0.2/option/mod.ts";
+export { batch } from "https://deno.land/x/denops_std@v5.0.2/batch/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v5.0.2/autocmd/mod.ts";
+export * as lambda from "https://deno.land/x/denops_std@v5.0.2/lambda/mod.ts";
