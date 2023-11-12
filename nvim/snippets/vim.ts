d1447649cd5afdfa9a45fcd68a3821ee32f04c17
uga-rosa <uga6603@gmail.com>
2023-11-12 20:26:22 +0900

    update snippet

---
+++ nvim/snippets/vim.ts
@@ -0,0 +1,18 @@
+import { TSSnippet } from "https://deno.land/x/denippet_vim@v0.0.4/loader.ts";
+import { Denops, fn } from "https://deno.land/x/denippet_vim@v0.0.4/deps/denops.ts";
+
+export const snippets: Record<string, TSSnippet> = {
+  "autoload function": {
+    prefix: "fn",
+    body: async (denops: Denops) => {
+      const path = await fn.expand(denops, "%:p") as string;
+      const match = path.match(/autoload\/(.+)\.vim$/);
+      const prefix = match ? match[1].replaceAll("/", "#") + "#" : "";
+      return [
+        `function ${prefix}$1($2) abort`,
+        "\t$0",
+        "endfunction",
+      ];
+    },
+  },
+};
