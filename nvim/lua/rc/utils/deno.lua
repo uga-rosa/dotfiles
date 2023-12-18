e38892602a3db78e8536a44027efb4de1d57328f
uga-rosa <uga6603@gmail.com>
2023-12-18 10:10:21 +0900

    rename

--- nvim/lua/rc/utils/deno.lua
+++ nvim/lua/rc/utils/deno.lua
@@ -3,7 +3,7 @@ local M = {}
 M.cache_pending = {}
 M.cache_ongoing = 0
 
-function M.cache(path)
+function M.cache_file(path)
   if M.cache_ongoing > 5 then
     M.cache_pending[path] = true
     return
@@ -18,26 +18,32 @@ function M.cache(path)
     end
     for pending, _ in pairs(M.cache_pending) do
       M.cache_pending[pending] = nil
-      M.cache(pending)
+      M.cache_file(pending)
     end
   end)
 end
 
-function M.cache_denops_plugins()
-  local root = require("lazy.core.config").options.root
-  for plugin in vim.fs.dir(root) do
+function M.cache_dir(dir)
+  for plugin in vim.fs.dir(dir) do
     vim.system(
-      { "fd", ".", "--type", "f", "-e", "ts" },
-      { cwd = vim.fs.joinpath(root, plugin) },
+      { "fd", ".", "denops", "--type", "f", "-e", "ts" },
+      { cwd = vim.fs.joinpath(dir, plugin) },
       function(fd)
-        if fd.stdout then
+        if fd.stdout ~= "" then
           for path in vim.gsplit(fd.stdout, "\n", { trimempty = true }) do
-            M.cache(vim.fs.joinpath(root, plugin, path))
+            M.cache_file(vim.fs.joinpath(dir, plugin, path))
           end
         end
       end
     )
   end
+end
+
+function M.cache_denops_plugins()
+  local root = require("lazy.core.config").options.root
+  local dev_root = require("lazy.core.config").options.dev.path
+  M.cache_dir(root)
+  M.cache_dir(dev_root)
 end
 
 return M
