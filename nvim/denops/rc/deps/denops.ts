db21255b4abc84e705954cdda5a8b2963c367016
uga-rosa <uga6603@gmail.com>
2023-12-28 08:53:09 +0900

    udd

--- nvim/denops/rc/deps/denops.ts
+++ nvim/denops/rc/deps/denops.ts
@@ -1,13 +1,13 @@
-export { type Denops } from "https://deno.land/x/denops_std@v5.1.0/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v5.1.0/function/mod.ts";
-export * as nvim from "https://deno.land/x/denops_std@v5.1.0/function/nvim/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v5.1.0/option/mod.ts";
-export { batch } from "https://deno.land/x/denops_std@v5.1.0/batch/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v5.1.0/autocmd/mod.ts";
-export * as lambda from "https://deno.land/x/denops_std@v5.1.0/lambda/mod.ts";
+export { type Denops } from "https://deno.land/x/denops_std@v5.2.0/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v5.2.0/function/mod.ts";
+export * as nvim from "https://deno.land/x/denops_std@v5.2.0/function/nvim/mod.ts";
+export * as op from "https://deno.land/x/denops_std@v5.2.0/option/mod.ts";
+export { batch } from "https://deno.land/x/denops_std@v5.2.0/batch/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v5.2.0/autocmd/mod.ts";
+export * as lambda from "https://deno.land/x/denops_std@v5.2.0/lambda/mod.ts";
 export {
   exprQuote as q,
   type ExprString,
   isExprString,
   useExprString,
-} from "https://deno.land/x/denops_std@v5.1.0/helper/expr_string.ts";
+} from "https://deno.land/x/denops_std@v5.2.0/helper/expr_string.ts";
