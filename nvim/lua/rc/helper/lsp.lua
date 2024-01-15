92a4de22aa1722d3285b6ffd39c50cbf3925272c
uga-rosa <uga6603@gmail.com>
2024-01-15 20:09:35 +0900

    fix: nil check and type

--- nvim/lua/rc/helper/lsp.lua
+++ nvim/lua/rc/helper/lsp.lua
@@ -2,13 +2,13 @@ local M = {}
 
 ---Register callback function on LspAttach
 ---@param name string|nil If nil, global
----@param callback fun(client:  table, bufnr: integer)
+---@param callback fun(client: lsp.Client, bufnr: integer)
 function M.on_attach(name, callback)
   vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(args)
       local bufnr = args.buf
       local client = vim.lsp.get_client_by_id(args.data.client_id)
-      if name == nil or client.name == name then
+      if client and (name == nil or client.name == name) then
         callback(client, bufnr)
       end
     end,
