17eabc297fa9c9b4a9f1789b8109cc5d7960db0c
uga-rosa <uga6603@gmail.com>
2024-02-05 01:36:16 +0900

    Add --unstable-kv

--- nvim/lua/rc/plugins/denops.lua
+++ nvim/lua/rc/plugins/denops.lua
@@ -2,6 +2,9 @@
 local spec = {
   "vim-denops/denops.vim",
   name = "denops.vim",
+  init = function()
+    vim.g["denops#server#deno_args"] = { "-q", "--no-lock", "-A", "--unstable-kv" }
+  end,
 }
 
 return spec
