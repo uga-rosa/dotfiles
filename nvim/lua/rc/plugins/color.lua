b89c9aecd88f7f82b03aad65c8ba05450829066a
uga-rosa <uga6603@gmail.com>
2024-03-19 17:31:04 +0900

    format

--- nvim/lua/rc/plugins/color.lua
+++ nvim/lua/rc/plugins/color.lua
@@ -7,7 +7,6 @@ local spec = {
       vim.cmd.colorscheme("nightfly")
 
       local palette = require("nightfly").palette
-
       vim.api.nvim_set_hl(0, "NormalFloat", { bg = palette.black })
       vim.api.nvim_set_hl(0, "FloatBorder", { fg = palette.white, bg = palette.black })
       vim.api.nvim_set_hl(0, "PmenuSel", { bg = palette.cyan_blue })
